e3e7c9278700a986d6f5f29c3bc6ef149f3ffe57
Dayane-Silva <Dayanevcarmo.s@gmail.com>
2024-03-12 22:15:17 -0300

    Mysql

--- API/server.js
+++ API/server.js
@@ -5,15 +5,23 @@ const cors = require('cors')
 require('dotenv').config()
 
 
-const server = express;
+const server = express();
 
-server.use(express.json());
+server.use(express.json()); 
 server.use(express.urlencoded({extended: true}));
 server.use(cors());
 server.use(bodyParser.json());
 
 
 const PORT = 7000
+
+const db = mysql.createConnection({
+    host: process.ev.DB_HOST,
+    host: process.ev.DB_PORT,
+    user: process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database: process.env.DB_NAME    
+}) 
 
 server.listen(PORT, () =>{
     console.log(`/O server está rodando em http:\\localhost${PORT}`)
