7ec32cd9cc3ccb55411e6f4e2a8bf3f191b5e714
Dayane-Silva <Dayanevcarmo.s@gmail.com>
2024-03-14 22:14:02 -0300

    Atividade 1 e 2

--- FRONTEND/Comment/CommentComponent.js
+++ FRONTEND/Comment/CommentComponent.js
@@ -1,45 +1,47 @@
-import { formatDate } from "../utils"
+import { formatDate } from "../utils.js";
+import { CommentService } from '../services/comment.services.js'
+import { Comment } from "../models/comment.model.js";
 
-const getInputComment = () =>{
-    return{
+const getInputComment = () => {
+    return {
         author: document.getElementById('inputAuthor'),
         comment: document.getElementById('inputComment')
     }
 }
 
-const setInputComment = (authorValue, commentValue) =>{
-    const {author, comment} = getInputComment();
+const setInputComment = (authorValue, commentValue) => {
+    const { author, comment } = getInputComment();
     author.value = authorValue
-    comment.value = commentValue 
+    comment.value = commentValue
 }
 
-const getInputCommentValue = () =>{
-    return{
-        author: document.getElementById('inputAuthor'),
-        comment: document.getElementById('inputComment')
+const getInputCommentValue = () => {
+    return {
+        author: document.getElementById('inputAuthor').value,
+        comment: document.getElementById('inputComment').value
     }
 }
-
 
 const submitComment = (event) => {
     event.preventDefault();
-
     const comment = getInputCommentValue()
 
-    //REQUISIÇÃO POST PARA ENVIAR COMMENT
-
-    data.push({ author: author, comment: comment })
-    console.log(data)
+    //requisção Post para enviar o comment
 
     loadComment()
 }
 
-
 const loadComment = () => {
     // Dados carregados da API
-    if (data) {
-        displayComment();
-    }
+    CommentService.apiGetComment().then(result => {
+        const comments = result.map(
+            (comment) => new Comment(comment.id, comment.author, comment.comment_text, comment.created_at, comment.updated_at)
+        );
+        displayComment(comments)
+    }).catch(error => {
+        console.error(error);
+        alert(error);
+    })
 }
 
 
@@ -48,34 +50,37 @@ const displayComment = (comments) => {
     divFeed.innerHTML = ``
     comments.forEach(item => {
         const divDisplay = document.createElement('div');
-        divDisplay.className = 'd-flex text-body-secondary pt-3'
+        divDisplay.className = 'd-flex text-body-secondary pt-3 border-bottom'
         divDisplay.innerHTML = `
             <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
                 xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
                 preserveAspectRatio="xMidYMid slice" focusable="false">
                 <title>comentário</title>
-                <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
-                    dy=".3em">32x32</text>
+                <rect width="100%" height="100%" fill="#444"></rect>
+                <text x="35%" y="50%" fill="#000000"dy=".3em">${item.getAuthor().charAt(0)}</text>
             </svg>
-            <p class="pb-3 mb-0 small lh-sm border-bottom">
-                <strong class="d-block text-gray-dark">@${item.author}
-                /<span class="date-style badge text-bg-secondary">${formatDate(item.date)}</span>
+            <p class="pb-3 mb-0 small lh-sm text-gray-dark">
+                <strong class="d-block text-gray-dark">@${item.getAuthor()}
+                <span class="date-style badge text-bg-secondary">${formatDate(item.getCreatedAt())}</span>
                 </strong>
-                /${item.comment}
+                <span class="comment">
+                ${item.getComment()}
+                </span>
             </p>        
         `
         divFeed.appendChild(divDisplay);
     })
 }
 
+
 const CommentComponent = {
-    run: () =>{
-        
+    run: () => {
         const formComentario = document.getElementById('formComment')
-         formComentario.addEventListener("submit", submitComment)
-         window.onload = () =>{
+        formComentario.addEventListener("submit", submitComment)
+        window.onload = () => {
             loadComment();
-         }
+        }
     }
 }
-export {CommentComponent}
+
+export { CommentComponent }
--- FRONTEND/utils.js
+++ FRONTEND/utils.js
@@ -1,14 +1,33 @@
-const getCurrentDataAndTime = () =>{
-    const currentDate = new Date();
-    const options = { 
+const formatDate = (data) => {
+    const currentDate = new Date(data);
+    const options = {
         day: '2-digit',
         month: 'short',
         hour: 'numeric',
         hour12: false
     };
 
-    let formattedDate = currentDate.toLocaleDateString('pt-BR',options)
+    let formattedDate = currentDate.toLocaleDateString('pt-BR', options)
     return formattedDate.replace(',', ' às') + 'hs';
 }
 
-export{formatDate}
+function randomColor(colors) {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+}
+
+function darkColors() { 
+    let darkPalette = ['#5a1d55', '#83447e','#ad6aa6', '#542251', '#bc3d72'];
+    return randomColor(darkPalette);
+    console.log(darkColors());
+}
+
+function lightColors() {
+    let lightPalette = ['#d691cf', '##ffb8f7', '##d095c9','#f7e6f4', '#ff9fd0'];
+    return randomColor(lightPalette);
+    console.log(lightColors());
+}
+
+
+export { formatDate, darkColors, lightColors }
+
