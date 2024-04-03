caec36cb1152af6de7d29a989671218cdcf7015e
Dayane-Silva <Dayanevcarmo.s@gmail.com>
2024-04-03 20:11:51 -0300

    "Implementação do Endpoint para Listagem de Comentários do Usuário

--- FRONTEND/Components/CommentComponent/CommentComponent.js
+++ FRONTEND/Components/CommentComponent/CommentComponent.js
@@ -1,21 +1,23 @@
 import { formatDate, randomColors } from "../../utils.js";
 import { CommentService } from '../../services/comment.services.js'
 import { Comment } from "../../models/comment.model.js";
-import { User } from "../../models/user.model.js";
+import { StorageServices } from "../../services/localStorage.service.js";
 
 
-const getInputComment = () => {
-    return {
-        author: document.getElementById('inputAuthor'),
-        comment: document.getElementById('inputComment')
-    }
+const getCommentInput = () => {
+    return document.getElementById('inputComment')
 }
-
-const setInputComment = (authorValue, commentValue) => {
-    const { author, comment } = getInputComment();
-    author.value = authorValue
+const getInputCommentValue = () => {
+    return document.getElementById('inputComment').value
+}
+const setInputComment = (commentValue) => {
+    const { comment } = getCommentInput();
     comment.value = commentValue
 }
+const clearCommentField = () => {
+    getCommentInput().value = ''
+}
+
 
 const setAuthorCommentField = (usr) => {
     const inputAuthor = document.getElementById('inputAuthor');
@@ -24,27 +26,19 @@ const setAuthorCommentField = (usr) => {
     inputAuthor.style.color = '#FFF'
 }
 
-const clearCommentField = () => {
-    const { comment } = getInputComment();
-    comment.value = ''
-}
-
-
-const getInputCommentValue = () => {
-    return {
-        author: document.getElementById('inputAuthor').value,
-        comment_text: document.getElementById('inputComment').value
-    }
-}
-
 const submitComment = (event) => {
     event.preventDefault();
-    const comment = getInputCommentValue(); 
-    CommentService.apiPostComment(comment).then(result => { 
-            alert(result)
-            clearCommentField();
-            loadComment();
-    }).catch((error) => { 
+
+    const comment = {
+        userId: StorageServices.user.get().getId(),
+        comment_text: getInputCommentValue()
+    };
+
+    CommentService.apiPostComment(comment).then(result => {
+        alert(result)
+        clearCommentField();
+        loadComment();
+    }).catch((error) => {
         console.log(error)
     });
 }
@@ -61,7 +55,6 @@ const loadComment = () => {
         alert(error);
     })
 }
-
 
 const displayComment = (comments) => {
     const divFeed = document.getElementById('comment-feed');
--- FRONTEND/Components/UserComponent/UserComponent.js
+++ FRONTEND/Components/UserComponent/UserComponent.js
@@ -1,8 +1,62 @@
 import { StorageServices } from "../../services/localStorage.service.js";
 import { randomColors } from "../../utils.js";
+import { CommentService } from '../../services/comment.services.js';
+import { formatDate } from "../../utils.js";
+
+const loadComment = (onlyUserComments = false) => {
+    // Dados carregados da API
+    CommentService.apiGetComment().then(result => {
+        const comments = result.map(
+            (comment) => new Comment(comment.id, comment.userId, comment.author, comment.comment_text, comment.created_at, comment.updated_at)
+        );
+        console.log("Comments from API:", comments);
+        
+        if (onlyUserComments) {
+            // Filtrar apenas os comentários do usuário logado
+            const userId = StorageServices.user.get().getId();
+            console.log("User ID:", userId);
+            
+            const userComments = comments.filter(comment => comment.userId === userId);
+            console.log("User Comments:", userComments);
+            
+            displayComment(userComments);
+        } else {
+            displayComment(comments);
+        }
+    }).catch(error => {
+        console.error(error);
+        alert(error);
+    })
+}
+
+const displayComment = (comments) => {
+    const divFeed = document.getElementById('comment-feed');
+    divFeed.innerHTML = ``
+    comments.forEach(item => {
+        const divDisplay = document.createElement('div');
+        divDisplay.className = 'd-flex text-body-secondary pt-3 border-bottom'
+        divDisplay.innerHTML = `
+            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+                preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>comentário</title>
+                <rect width="100%" height="100%" fill="#${randomColors().dark}"></rect>
+                <text x="35%" y="50%" fill="#${randomColors().light}"dy=".3em">${item.getAuthor().charAt(0)}</text>
+            </svg>
+            <p class="pb-3 mb-0 small lh-sm text-gray-dark">
+                <strong class="d-block text-gray-dark">@${item.getAuthor()}
+                <span class="date-style badge text-bg-secondary">${formatDate(item.getCreatedAt())}</span>
+                </strong>
+                <span class="comment">
+                ${item.getComment()}
+                </span>
+            </p>        
+        `
+        divFeed.appendChild(divDisplay);
+    })
+}
 
 const loadUserData = () => {
-
     displayUserData(StorageServices.user.get())
 }
 
@@ -76,7 +130,12 @@ const UserComponent = {
         btnMeusDados.addEventListener('click', handleShowHideUser);
         const btnSairMDados = document.getElementById('btnSairMDados');
         btnSairMDados.addEventListener('click', handleShowHideUser);
+        
+        const btnMeusComentariosMDados = document.getElementById('btnMeusComentariosMDados');
+        btnMeusComentariosMDados.addEventListener('click', function() {
+            loadComment(true); 
+        });
     }
 }
 
-export { UserComponent }
+export { UserComponent };
